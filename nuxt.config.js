73329ad191dd81a138c7a3429371de79336582f8
aeh1307 <astridehamre@gmail.com>
2020-08-22 13:42:20 +0200

    Removed import from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,27 +91,24 @@ export default {
         }
     }
   },
-vuetify: {
-  customVariables: ['~/assets/variables.scss'],
-    theme:
-  {
-    light: true,
-      themes:
-    {
-      light: {
-        primary: '#1976D2',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-        white: '#FEFEFE',
+  vuetify: {
+    theme: {
+      light: true,
+        themes:
+      {
+        light: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+          white: '#FEFEFE',
+        }
       }
     }
-  }
-}
-,
+  },
 /*
 ** Build configuration
 */
