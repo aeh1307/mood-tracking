46b715643fd7867b1aba844ed8c3981fb1ddd867
aeh1307 <astridehamre@gmail.com>
2020-09-13 10:16:48 +0200

    Fixed transform issue in iOS

--- components/moodTracker/MoodTracker.vue
+++ components/moodTracker/MoodTracker.vue
@@ -248,24 +248,31 @@
 
   li:first-child {
     transform: rotate(0deg) skewY(-39deg);
+    -webkit-transform:rotate(0deg) skewY(-39deg);
   }
   li:nth-child(2) {
     transform: rotate(51.4285714286deg) skewY(-39deg);
+    -webkit-transform: rotate(51.4285714286deg) skewY(-39deg);
   }
   li:nth-child(3) {
     transform: rotate(102.8571428572deg) skewY(-39deg);
+    -webkit-transform: rotate(102.8571428572deg) skewY(-39deg);
   }
   li:nth-child(4) {
     transform: rotate(154.2857142858deg) skewY(-39deg);
+    -webkit-transform: rotate(154.2857142858deg) skewY(-39deg);
   }
   li:nth-child(5) {
     transform: rotate(205.7142857144deg) skewY(-39deg);
+    -webkit-transform: rotate(205.7142857144deg) skewY(-39deg);
   }
   li:nth-child(6) {
     transform: rotate(257.142857143deg) skewY(-39deg);
+    -webkit-transform: rotate(257.142857143deg) skewY(-39deg);
   }
   li:nth-child(7) {
     transform: rotate(308.5714285716deg) skewY(-39deg);
+    -webkit-transform: rotate(308.5714285716deg) skewY(-39deg);
   }
   li:first-child .circlePart {
     background: #ffffff;
